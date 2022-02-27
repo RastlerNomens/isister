d0e1959dbed465f398dd7490906e68a46b405e4f
Dani Molina (Rastler) <rastler89@gmail.com>
2022-02-27 22:43:40 +0100

    Add new dashboard

--- resources/views/beastmaster/users.blade.php
+++ resources/views/beastmaster/users.blade.php
@@ -9,6 +9,7 @@
         <th scope="col">Google ID</th>
         <th scope="col">Zoologic?</th>
         <th scope="col">Roles</th>
+        <th scope="col">Actions</th>
     </thead>
     <tbody>
     @foreach ($users as $user)
@@ -23,8 +24,31 @@
                     {{$role->name}}
                 @endforeach
             </td>
+            <td>
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                <i class="fal fa-eye"></i>
+                </button>
+            </td>
         </tr>
     @endforeach
     </tbody>
 </table>
+
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
 @endsection
--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -1,95 +1,40 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <!-- CSRF Token -->
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-
-        <title>{{ config('app.name', 'Isister') }}</title>
-
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
-
-        <!-- Fonts -->
-        <link rel="dns-prefetch" href="//fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet"/>
-
-        <!-- Styles -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <link href="{{ asset('css/dashboard.css') }}" rel="stylesheet">
-
-    </head>
-    <body>
-        <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-            <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Isister</a>
-            <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
-            <div class="navbar-nav">
-                <div class="nav-item text-nowrap">
-                    <a class="nav-link px-3" href="{{ route('logout') }}"
-                        onclick="event.preventDefault();
-                        document.getElementById('logout-form').submit();">
-                        <i class="fas fa-sign-out-alt"></i>
-                    </a>                
-
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                    @csrf
-                    </form>
-                </div>
-            </div>
-        </header>
-
-        <div class="container-fluid">
-            <div class="row">
-                <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-                    <div class="position-sticky pt-3">
-                        <ul class="nav flex-column">
-                            <li class="nav-item">
-                                <a class="nav-link @if (request()->is('admin')) active @endif" href="{{ route('beastmaster.dashboard') }}" aria-current="page">
-                                    <i class="fas fa-home"></i>
-                                    Dashboard
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link @if (request()->is('admin/users')) active @endif" href="{{ route('beastmaster.getUsers') }}" aria-current="page">
-                                    <i class="fas fa-user"></i>
-                                    Users
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#" aria-current="page">
-                                    <i class="fas fa-user-tag"></i>
-                                    Roles
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#" aria-current="page">
-                                    <i class="fas fa-user"></i>
-                                    Enfermedades
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#" aria-current="page">
-                                    <i class="fas fa-tasks"></i>
-                                    Functions
-                                </a>
-                            </li>
-                            
-                            
-                        </ul>
-                    </div>
-                </nav>
-
-                <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-                    @yield('content')
-                </main>
-            </div>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Isister - Beast</title>
+    <link rel="stylesheet" href="{{ asset('dash/vendors/mdi/css/materialdesignicons.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('dash/vendors/css/vendor.bundle.base.css') }}">
+    <link rel="stylesheet" href="{{ asset('dash/vendors/jvectormap/jquery-jvectormap.css') }}">
+    <link rel="stylesheet" href="{{ asset('dash/vendors/flag-icon-css/css/flag-icon.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('dash/vendors/owl-carousel-2/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('dash/vendors/owl-carousel-2/owl.theme.default.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('dash/css/style.css') }}">
+    <link rel="shortcut icon" href="{{ asset('dash/images/favicon.png') }}" />
+  </head>
+  <body>
+    <div class="container-scroller">
+      @include('partials.sidebar')
+      <div class="container-fluid page-body-wrapper">
+        @include('partials.navbar')
+        <div class="main-panel">
+          @yield('content')
+          @include('partials.footer')
         </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    </body>
+      </div>
+    </div>
+    <script src="{{ asset('dash/vendors/js/vendor.bundle.base.js') }}"></script>
+    <script src="{{ asset('dash/vendors/chart.js/Chart.min.js') }}"></script>
+    <script src="{{ asset('dash/vendors/progressbar.js/progressbar.min.js') }}"></script>
+    <script src="{{ asset('dash/vendors/jvectormap/jquery-jvectormap.min.js') }}"></script>
+    <script src="{{ asset('dash/vendors/jvectormap/jquery-jvectormap-world-mill-en.js') }}"></script>
+    <script src="{{ asset('dash/vendors/owl-carousel-2/owl.carousel.min.js') }}"></script>
+    <script src="{{ asset('dash/js/off-canvas.js') }}"></script>
+    <script src="{{ asset('dash/js/hoverable-collapse.js') }}"></script>
+    <script src="{{ asset('dash/js/misc.js') }}"></script>
+    <script src="{{ asset('dash/js/settings.js') }}"></script>
+    <script src="{{ asset('dash/js/todolist.js') }}"></script>
+    <script src="{{ asset('dash/js/dashboard.js') }}"></script>
+  </body>
 </html>
